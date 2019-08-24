b3f13bbaf203865f6a088a47dbc2cfeae1896aea
Vaibhav Sharma <vsvaibhav2016@gmail.com>
2019-08-24 14:12:32 +0530

    saved fav list

--- lib/main.dart
+++ lib/main.dart
@@ -71,11 +71,35 @@ class RandomWordsState extends State<RandomWords> {
       );
   }
 
+
   @override
   Widget build(BuildContext conext) {
+    void _pushSaved() {
+      Navigator.of(context).push(
+        MaterialPageRoute<void>(
+          builder: (BuildContext context) {
+            final Iterable<ListTile> tiles = _saved.map(
+              (WordPair pair) {
+                return ListTile(
+                  title: Text(
+                    pair.asPascalCase,
+                    style: _biggerFont,
+                  ),
+                );
+              },
+            );
+            final List<Widget> divided = ListTile.divideTiles(context: conext,tiles: tiles).toList();
+            return Scaffold(appBar: AppBar(title: Text('Saved Suggestions'),),body: ListView(children: divided));
+          }
+        )
+      );
+    }
     return Scaffold (                   // Add from here... 
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved,)
+        ],
       ),
       body: _buildSuggestions(),
     );
